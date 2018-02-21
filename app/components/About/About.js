e031a0c39efbc793fe3d83f29514475988ba9b23
Greg Griffith <griffith@cryptounited.io>
2018-02-20 16:03:15 -0800

    redo the about page so it isnt so messy

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -21,30 +21,38 @@ export default class About extends Component {
           <p className="title">{lang.aboutTitle}</p>
           <div className="panel panel-default">
             <div className="panel-body text-center larger-text">
-              <div className="col-md-12 col-lg-12 col-xs-12 " style={{ marginBottom: '25px' }}>
-                <p>This User Interface for {config.coinName} is built off of the Sapling base created by <a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, 'https://www.cryptounited.io/')}>CryptoUnited 2017-2018 &#169;</a></p>
-              </div>
-              <div className="col-md-6 col-lg-6 col-xs-6" style={{ cursor: 'pointer' }}>
-                <a onClick={this.openLink.bind(this, config.bitcointalkLink)} className="section">
-                  <img src={bitcoinImage} style={{ width: '50%' }} alt="" />
-                  <span className="sub">{config.bitcointalkText}</span>
-                </a>
+              <div className="col-md-12 col-lg-12 col-xs-12">
+                <h2>{config.coinName}</h2>
               </div>
               <div className="col-md-6 col-lg-6 col-xs-6">
-                <img src={githubImage} style={{ width: '50%' }} alt="" />
+                <img src={bitcoinImage} style={{ width: '25%' }} alt="" />
+                <h3><a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, config.bitcointalkLink)}>{config.bitcointalkText}</a></h3>
+              </div>
+              <div className="col-md-6 col-lg-6 col-xs-6">
+                <img src={githubImage} style={{ width: '25%' }} alt="" />
                 <div className="section">
-                  <a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, config.githubLink)}> {config.githubText} </a>
+                  <h3><a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, config.githubLink)}> {config.githubText} </a></h3>
                 </div>
               </div>
             </div>
-            <div style={{marginTop:'25px'}} className="panel panel-default">
-                <div className="panel-body text-center larger-text">
-                    <div className="col-md-12 col-lg-12 col-xs-12">
-                        <p>The Sapling code base for this user interface is brought to you free of charge by the engineers at CryptoUnited. </p>
-                        <p>The Sapling github can be found <a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, 'https://www.github.com/cryptounited/sapling')}> HERE. </a></p>
-                        <p>If you like this product and would like to show your support for future development of this and other free software, you may send a donation to</p> <p>BTC: 1LWdVxMkdYDSJKoLEXddUKFh5b2AWqu4M6</p> <p>BCH: 1LWdVxMkdYDSJKoLEXddUKFh5b2AWqu4M6</p>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div className="col-md-12">
+          <div className="panel panel-default">
+            <div className="panel-body text-center larger-text">
+              <div className="col-md-12 col-lg-12 col-xs-12 " style={{ marginBottom: '25px' }}>
+                <h4>This User Interface for {config.coinName} is built off of the Sapling code base created by <a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, 'https://www.cryptounited.io/')}>CryptoUnited 2017-2018 &#169;</a></h4>
+                <p>The Sapling github can be found <a style={{ cursor: 'pointer' }} onClick={this.openLink.bind(this, 'https://www.github.com/cryptounited/sapling')}> HERE. </a></p>
+              </div>
+            </div>
+            <div className="panel-body larger-text">
+              <div className="col-md-12 col-lg-12 col-xs-12">
+                <p>If you like this project and want to show your support for future development of this and other free software, you may send a donation to</p>
+              </div>
+              <div className="col-md-12 col-lg-12 col-xs-12">
+                <p>BTC: 1LWdVxMkdYDSJKoLEXddUKFh5b2AWqu4M6</p> 
+                <p>BCH: 1LWdVxMkdYDSJKoLEXddUKFh5b2AWqu4M6</p>
+              </div>
             </div>
           </div>
         </div>
