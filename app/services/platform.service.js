57245d5eb3f5919e160306adcc0a89ef239f140d
Gl3nda85 <s3249319@student.rmit.edu.au>
2018-04-07 22:28:59 +1000

    fixed all generics and added more variables to the config file to handle all this.

--- app/services/platform.service.js
+++ app/services/platform.service.js
@@ -1,24 +1,45 @@
 import config from '../../config.json';
 
 const homedir = require('os').homedir();
-const releaseUrl = config.releaseUrl;
 const os = require('os');
+
+/**
+ * This function returns the directory Uri for the wallet daemon and version files.
+ * @returns {string}
+ */
 
 export function grabWalletDir() {
   if (process.platform === 'linux') {
     // linux directory
-    return `${homedir}/.eccoin-wallet/`;
+    return `${homedir}/.${config.daemonFolder}/`;
   } else if (process.platform === 'darwin') {
     // OSX
-    return `${homedir}/Library/Application Support/.eccoin-wallet/`;
+    return `${homedir}/Library/Application Support/.${config.daemonFolder}/`;
   } else if (process.platform.indexOf('win') > -1) {
     // Windows
-    return `${homedir}\\.eccoin-wallet\\`;
+    return `${homedir}\\.${config.daemonFolder}\\`;
   }
 }
+
+/**
+ * This function forwards the daemon zip download url given the inputted parameters.
+ * @param product
+ * @param version
+ * @param platform
+ * @returns {string}
+ */
+
 export function formatDownloadURL(product, version, platform) {
   return `https://github.com/project-ecc/${product}/releases/download/${version}/${product}-${version}-${platform}.zip`;
 }
+
+
+/**
+ * Extracts the platform zip checksum from the body of text on the github release page.
+ * @param platform
+ * @param text
+ * @returns {string}
+ */
 
 export function extractChecksum (platform, text) {
   // kinda shitty...node doesn't have dotall (s flag) as of this code...whitespace/not whitespace is a hack...
@@ -27,53 +48,77 @@ export function extractChecksum (platform, text) {
   return (checksumMatches && checksumMatches.length > 1) ? checksumMatches[1] : '';
 }
 
-export function getPlatformFileName() {
+/**
+ * Returns the daemons file name, Works accross platforms.
+ * @returns {string}
+ */
 
+export function getPlatformFileName() {
   if (process.platform === 'linux') {
 
-    return os.arch() === 'x32' ? config.linux32 : config.linux64;
+    return os.arch() === 'x32' ? `${config.daemonName}-linux32` : `${config.daemonName}-linux64`;
 
   } else if (process.platform === 'darwin') {
 
-    return config.osx;
+    return `${config.daemonName}.app`;
 
   } else if (process.platform.indexOf('win') > -1) {
 
-    return os.arch() === 'x32' ? config.win32 : config.win64;
+    return os.arch() === 'x32' ? `${config.daemonName}-win32.exe` : `${config.daemonName}-win64.exe`;
   }
 }
 
-export function grabEccoinDir() {
+/**
+ * returns the coin Dir which contains the block Db, wallet.dat and conf files.
+ * @returns {string}
+ */
+
+export function grabCoinDir() {
   if (process.platform === 'linux') {
     // linux directory
-    return `${homedir}/.eccoin/`;
+    return `${homedir}/.${config.resourceFolder}/`;
   } else if (process.platform === 'darwin') {
     // OSX
-    return `${homedir}/Library/Application Support/eccoin/`;
+    return `${homedir}/Library/Application Support/${config.resourceFolder}/`;
   } else if (process.platform.indexOf('win') > -1) {
     // Windows
-    return `${homedir}\\Appdata\\roaming\\eccoin\\`;
+    return `${homedir}\\Appdata\\roaming\\${config.resourceFolder}\\`;
   }
 }
+
+/**
+ * Returns the full path to the wallet daemon.
+ * @returns {string}
+ */
 
 export function getPlatformWalletUri() {
   if (process.platform === 'linux') {
     // linux directory
-    return `${grabWalletDir()}Eccoind`;
+    return `${grabWalletDir()}${getPlatformFileName()}`;
   } else if (process.platform === 'darwin') {
     // OSX
-    return `${grabWalletDir()}Eccoind.app/Contents/MacOS/eccoind`;
+    return `${grabWalletDir()}${getPlatformFileName()}/Contents/MacOS/${config.daemonName}`;
   } else if (process.platform.indexOf('win') > -1) {
     // Windows
-    return `${grabWalletDir()}Eccoind.exe`;
+    return `${grabWalletDir()}${getPlatformFileName()}`;
   }
 }
 
+/**
+ * Returns the conf file full path.
+ * @returns {string}
+ */
+
 export function getConfUri() {
-  return `${grabEccoinDir()}eccoin.conf`;
+  return `${grabCoinDir()}eccoin.conf`;
 }
 
+/**
+ * returns the debug file full path.
+ * @returns {string}
+ */
+
 export function getDebugUri() {
-  return `${grabEccoinDir()}debug.log`;
+  return `${grabCoinDir()}debug.log`;
 }
 
