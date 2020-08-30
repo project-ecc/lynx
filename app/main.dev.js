b7cc16ebac55971278a51869b6b4276979741fa5
Greg-Griffith <probablyaplebeian@protonmail.com>
2020-08-29 18:56:48 -0700

    remove unused autoupdater code

--- app/main.dev.js
+++ app/main.dev.js
@@ -16,7 +16,6 @@ import { grabWalletDir } from './services/platform.service';
 
 const { app, Tray, Menu, BrowserWindow, nativeImage, ipcMain } = require('electron');
 
-const autoUpdater = require('electron-updater').autoUpdater;
 const settings = require('electron-settings');
 const event = require('./utils/eventhandler');
 const { logo } = require('./base-sixty-four');
@@ -32,36 +31,6 @@ function sendStatusToWindow(text) {
   console.log(text);
   mainWindow.webContents.send('message', text);
 }
-
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-});
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-});
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-});
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater.');
-});
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = `Download speed: ${progressObj.bytesPerSecond}`;
-  log_message = `${log_message} - Downloaded ${progressObj.percent}%`;
-  log_message = `${log_message} (${progressObj.transferred}/${progressObj.total})`;
-  sendStatusToWindow(log_message);
-});
-
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded; will install in 5 seconds');
-  // Wait 5 seconds, then quit and install
-  // In your application, you don't need to wait 5 seconds.
-  // You could call autoUpdater.quitAndInstall(); immediately
-  setTimeout(() => {
-    autoUpdater.quitAndInstall();
-  }, 5000);
-});
-
 
 let mainWindow = null;
 
@@ -190,7 +159,6 @@ app.on('ready', async () => {
       mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
     });
   }
-  autoUpdater.checkForUpdates();
 });
 
 ipcMain.on('wallet-version-created', (e, args) => {
